4ae55298479c557c5b0c81cbde60089186adc011
Pranjalkumar Patel <patelpranzp25@gmail.com>
2019-01-14 10:22:38 -0800

    Update SinglyLinkedList.java

--- LinkedList/Singly/SinglyLinkedList.java
+++ LinkedList/Singly/SinglyLinkedList.java
@@ -1,23 +1,15 @@
 package linkedList;
 
 public class SinglyLinkedList{
-
-// head Node
 	private static Node head = null;
 	
 	public static void main(String[] args) {
-		
-    // insertion call
-    insert(1,10);
+		insert(1,10);
 		insert(2,20);
 		insert(2,30);
 		insert(1,40);
 		insert(5,50);
-    
-    // print a list
 		print();
-    
-    // delete element, position passed parameter
 		delete(1);
 		System.out.println();
 		print();
@@ -27,62 +19,16 @@ public class SinglyLinkedList{
 		delete(2);
 		System.out.println();
 		print();
-    
-    // delete the list
 		deleteTheList();
 	}
 
-  // insertion of element in list based at specific position
-	private static void insert(int position, int element) {
-  
-    // checking corner cases for position
-		if(head == null || position > 0 && position <= lengthOfList()+1) {
-			Node node = new Node(element);
-			Node temp = head; int k = 1 ;
-      
-      // if list is empty
-			if(temp == null) {
-				head = node;
-        
-      // insertion at first position
-			} else if(position == 1){
-				node.next = temp;
-				head = node;	
-        
-      // insertion at nth place(except 1)
-			} else {
-				while( temp != null && k  < position-1) {
-					temp = temp.next;
-					k++;
-				}
-				Node nextOfTemp = temp.next;
-				temp.next = node;
-				
-        // condition to check if  insertion at last
-        if(nextOfTemp != null) {
-					node.next = nextOfTemp;
-				}
-			}
-		} else {
-			System.out.println(" Cant insert due to wrong position ... ");
-		}
-	}
-
-
-
-  // delete any element's method
 	private static void delete(int del) {
-  
-    // cheking corner cases for position to delete
 		if(del > 0 && del <= lengthOfList()) {
 			Node temp = head;
 			int k = 1;
-      
-      // position to delete is one
 			if(del == 1) {
 				head = temp.next;
 				temp = null;
-       // traverse upto node previous than to be deleted node
 			} else {
 				while(temp != null && k < del-1) {
 					temp = temp.next;
@@ -93,8 +39,6 @@ public class SinglyLinkedList{
 		}
 	}
 
-
-  //   delete the entire list
 	private static void deleteTheList() {
 		Node temp = head, dell;
 		
@@ -106,7 +50,31 @@ public class SinglyLinkedList{
 		head = null;
 	}
 
-  // find length of the list
+	private static void insert(int position, int element) {
+		if(head == null || position > 0 && position <= lengthOfList()+1) {
+			Node node = new Node(element);
+			Node temp = head; int k = 1 ;
+			if(temp == null) {
+				head = node;
+			} else if(position == 1){
+				node.next = temp;
+				head = node;	
+			} else {
+				while( temp != null && k  < position-1) {
+					temp = temp.next;
+					k++;
+				}
+				Node nextOfTemp = temp.next;
+				temp.next = node;
+				if(nextOfTemp != null) {
+					node.next = nextOfTemp;
+				}
+			}
+		} else {
+			System.out.println(" Cant insert due to wrong position ... ");
+		}
+	}
+
 	private static int lengthOfList() {
 		int count = 0;
 		
@@ -122,7 +90,6 @@ public class SinglyLinkedList{
 		return count;
 	} 
 		
-  // print the list  
 	private static void print() {
 		Node temp = head;
 		if(temp == null) {
@@ -134,5 +101,30 @@ public class SinglyLinkedList{
 				temp = temp.next ;
 			}
 		}
+	}
+	
+	private static void ReverseIterative() {
+		Node a = head, b = head, c = null;
+		while( b != null) {
+			a = b.next;
+			b.next = c;
+			c = b;
+			b = a;
+		}
+		// print(c);
+	}
+	
+	private static SinglyNode ReversePrintRecursive(SinglyNode node) {
+		SinglyNode newNode;
+		
+		if(node.next == null) {
+			return node;
+		}
+		
+		newNode = ReversePrintRecursive(node.next);
+		
+		node.next.next = node;
+		node.next = null;
+		return newNode;
 	}
 }
